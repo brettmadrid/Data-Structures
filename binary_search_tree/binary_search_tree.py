5e38c1a997f7862766c2f93e6ad66b66eb5efb5c
brettmadrid <brettmadrid9@gmail.com>
2019-12-22 21:36:56 -0800

    added contains method to binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -34,7 +34,20 @@ class BinarySearchTree:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # if target == self.value, return it
+        if target == self.value:
+            return True
+        # go left or right based on smaller or bigger
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
